f7238d732d54d716034ef37cbf852f5010ab0284
Chiziaruhoma Ogbonda <chiziaruhoma@gmail.com>
2025-11-27 03:22:35 +0100

    Add streaming generate recipe endpoint

--- magic_recipe_server/lib/src/generated/endpoints.dart
+++ magic_recipe_server/lib/src/generated/endpoints.dart
@@ -424,6 +424,34 @@ class Endpoints extends _i1.EndpointDispatch {
                     params['path'],
                   ),
         ),
+        'generateRecipeStream': _i1.MethodStreamConnector(
+          name: 'generateRecipeStream',
+          params: {
+            'ingredients': _i1.ParameterDescription(
+              name: 'ingredients',
+              type: _i1.getType<String>(),
+              nullable: false,
+            ),
+            'imagePath': _i1.ParameterDescription(
+              name: 'imagePath',
+              type: _i1.getType<String?>(),
+              nullable: true,
+            ),
+          },
+          streamParams: {},
+          returnType: _i1.MethodStreamReturnType.streamType,
+          call:
+              (
+                _i1.Session session,
+                Map<String, dynamic> params,
+                Map<String, Stream> streamParams,
+              ) => (endpoints['recipes'] as _i5.RecipesEndpoint)
+                  .generateRecipeStream(
+                    session,
+                    params['ingredients'],
+                    params['imagePath'],
+                  ),
+        ),
       },
     );
     modules['serverpod_auth_idp'] = _i7.Endpoints()
