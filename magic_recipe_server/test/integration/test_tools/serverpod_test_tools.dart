9aae8ff467b44e8492f490c1ab6c9a237aa59017
Chiziaruhoma Ogbonda <chiziaruhoma@gmail.com>
2025-11-27 03:22:32 +0100

    Add integration tests for generateRecipe

--- magic_recipe_server/test/integration/test_tools/serverpod_test_tools.dart
+++ magic_recipe_server/test/integration/test_tools/serverpod_test_tools.dart
@@ -16,6 +16,8 @@ import 'package:serverpod_test/serverpod_test.dart' as _i1;
 import 'package:serverpod/serverpod.dart' as _i2;
 import 'dart:async' as _i3;
 import 'package:magic_recipe_server/src/generated/greeting.dart' as _i4;
+import 'package:magic_recipe_server/src/generated/recipes/models/recipe.dart'
+    as _i5;
 import 'package:magic_recipe_server/src/generated/protocol.dart';
 import 'package:magic_recipe_server/src/generated/endpoints.dart';
 export 'package:serverpod_test/serverpod_test_public_exports.dart';
@@ -198,7 +200,7 @@ class _RecipesEndpoint {
 
   final _i2.SerializationManager _serializationManager;
 
-  _i3.Future<String> generateRecipe(
+  _i3.Future<_i5.Recipe> generateRecipe(
     _i1.TestSessionBuilder sessionBuilder,
     String ingredients,
   ) async {
@@ -221,7 +223,37 @@ class _RecipesEndpoint {
                   _localUniqueSession,
                   _localCallContext.arguments,
                 )
-                as _i3.Future<String>);
+                as _i3.Future<_i5.Recipe>);
+        return _localReturnValue;
+      } finally {
+        await _localUniqueSession.close();
+      }
+    });
+  }
+
+  _i3.Future<List<_i5.Recipe>> getRecipes(
+    _i1.TestSessionBuilder sessionBuilder,
+  ) async {
+    return _i1.callAwaitableFunctionAndHandleExceptions(() async {
+      var _localUniqueSession =
+          (sessionBuilder as _i1.InternalTestSessionBuilder).internalBuild(
+            endpoint: 'recipes',
+            method: 'getRecipes',
+          );
+      try {
+        var _localCallContext = await _endpointDispatch.getMethodCallContext(
+          createSessionCallback: (_) => _localUniqueSession,
+          endpointPath: 'recipes',
+          methodName: 'getRecipes',
+          parameters: _i1.testObjectToJson({}),
+          serializationManager: _serializationManager,
+        );
+        var _localReturnValue =
+            await (_localCallContext.method.call(
+                  _localUniqueSession,
+                  _localCallContext.arguments,
+                )
+                as _i3.Future<List<_i5.Recipe>>);
         return _localReturnValue;
       } finally {
         await _localUniqueSession.close();
