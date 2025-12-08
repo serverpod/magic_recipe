def02d1bb741bda01cea7c8166640a3714588b5c
Chiziaruhoma Ogbonda <chiziaruhoma@gmail.com>
2025-12-08 03:54:32 +0100

    Store Recipe in database

--- magic_recipe_server/lib/src/recipes/services/recipe_ai_service.dart
+++ magic_recipe_server/lib/src/recipes/services/recipe_ai_service.dart
@@ -43,7 +43,8 @@ Always put the title of the recipe in the first line, and then the instructions.
       ingredients: ingredients,
     );
 
-    return recipe;
+    final recipeWithId = await Recipe.db.insertRow(session, recipe);
+    return recipeWithId;
   }
 
   String _buildTextPrompt(String ingredients) {
