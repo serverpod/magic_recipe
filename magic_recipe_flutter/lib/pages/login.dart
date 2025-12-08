8a8182812e3be1bcc693bae6c503a7948f675b22
Chiziaruhoma Ogbonda <chiziaruhoma@gmail.com>
2025-12-08 04:50:19 +0100

    Add auth module to app

---
+++ magic_recipe_flutter/lib/pages/login.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:magic_recipe_flutter/extensions/extensions.dart';
+import 'package:magic_recipe_flutter/main.dart';
+import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SignInWidget(
+        client: client,
+        // NOTE: No need to call navigation here if it gets done on the
+        // client.auth.authInfo listener.
+        onAuthenticated: () => onAuthenticated(context),
+        onError: (error) => onError(context, error),
+      ),
+    );
+  }
+
+  void onAuthenticated(BuildContext context) {
+    context.showSnackBar(
+      message: 'User authenticated.',
+      backgroundColor: Colors.green,
+    );
+  }
+
+  void onError(BuildContext context, Object error) {
+    context.showSnackBar(
+      message: 'Authentication failed: $error',
+      backgroundColor: Colors.red,
+    );
+  }
+}
