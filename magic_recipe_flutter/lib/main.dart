21a79e7a81f42aae3d1d7b3033165feb5b714271
Chiziaruhoma Ogbonda <chiziaruhoma@gmail.com>
2026-03-26 12:00:00 +0000

    Add auth module to app

--- magic_recipe_flutter/lib/main.dart
+++ magic_recipe_flutter/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:magic_recipe_client/magic_recipe_client.dart';
 import 'package:flutter/material.dart';
+import 'package:magic_recipe_flutter/pages/pages.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
+import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
 
 /// Sets up a global client object that can be used to talk to the server from
 /// anywhere in our app. The client is generated from your server code
@@ -12,13 +14,14 @@ import 'package:serverpod_flutter/serverpod_flutter.dart';
 late final Client client;
 
 late String serverUrl;
-
-void main() {
+void main() async {
   // When you are running the app on a physical device, you need to set the
   // server URL to the IP address of your computer. You can find the IP
   // address by running `ipconfig` on Windows or `ifconfig` on Mac/Linux.
   // You can set the variable when running or building your app like this:
   // E.g. `flutter run --dart-define=SERVER_URL=https://api.example.com/`
+  WidgetsFlutterBinding.ensureInitialized();
+
   const serverUrlFromEnv = String.fromEnvironment('SERVER_URL');
   final serverUrl = serverUrlFromEnv.isEmpty
       ? 'http://$localhost:8080/'
@@ -27,18 +30,52 @@ void main() {
   client = Client(serverUrl)
     ..connectivityMonitor = FlutterConnectivityMonitor();
 
+  final sessionManager = FlutterAuthSessionManager();
+  client.authSessionManager = sessionManager;
+  await sessionManager.initialize();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // NOTE: This is the only required setState to ensure that the  UI gets
+    // updated when the auth state changes.
+    client.auth.authInfoListenable.addListener(_updateSignedInState);
+  }
+
+  @override
+  void dispose() {
+    client.auth.authInfoListenable.removeListener(_updateSignedInState);
+    super.dispose();
+  }
+
+  void _updateSignedInState() {
+    setState(() {
+      _isSignedIn = client.auth.isAuthenticated;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Serverpod Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(title: 'Serverpod Example'),
+      home: _isSignedIn
+          ? const MyHomePage(title: 'Serverpod Example')
+          : const LoginPage(),
     );
   }
 }
